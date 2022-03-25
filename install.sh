4072e10c27f81d60008af35330eb5884b533ce6a
developerSid <gary.w.myers@outlook.com>
2022-03-24 23:13:51 -0500

    Add neovim setup

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+mkdir -p ~/.config/nvim/
+
 cd ~
 ln -sf ~/.dotfiles/vimrc .vimrc
 ln -sf ~/.dotfiles/zshrc .zshrc
 ln -sf ~/.dotfiles/gitconfig .gitconfig
+ln -sf ~/.dotfiles/init.vim ~/.config/nvim/init.vim
