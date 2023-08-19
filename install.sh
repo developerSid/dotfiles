e1ff658ab548c1313be0067f1e2d80d3cf838239
developerSid <gary.w.myers@outlook.com>
2023-08-19 00:23:25 -0500

    Redid zellij config
    removed old vim setup

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/.config/nvim/ ~/.config/alacritty/
+mkdir -p ~/.config/nvim/ ~/.config/alacritty/ ~/.config/zellij
 
 cd ~
 ln -sf ~/.dotfiles/vimrc .vimrc
@@ -8,3 +8,4 @@ ln -sf ~/.dotfiles/zshrc .zshrc
 ln -sf ~/.dotfiles/gitconfig .gitconfig
 ln -sf ~/.dotfiles/init.vim ~/.config/nvim/init.vim
 ln -sf ~/.dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -sf ~/.dotfiles/zellij.kdl ~/.config/zellij/config.kdl
