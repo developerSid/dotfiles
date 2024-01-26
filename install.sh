4103e31afe129c89f9ec6a8dc16abf4a24222ba6
CommonFollower <gary.w.myers@outlook.com>
2024-01-26 15:01:07 -0600

    Converted alacritty config from yaml to toml

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ mkdir -p ~/.config/alacritty/ ~/.config/zellij
 cd ~
 ln -sf ~/.dotfiles/zshrc .zshrc
 ln -sf ~/.dotfiles/gitconfig .gitconfig
-ln -sf ~/.dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -sf ~/.dotfiles/alacritty.toml ~/.config/alacritty/alacritty.toml
 ln -sf ~/.dotfiles/zellij.kdl ~/.config/zellij/config.kdl
 
 if [ -d "$HOME/.sdkman/etc/" ]; then
